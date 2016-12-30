31edd49d70d7cae0d863680df22ce1d420fa2452
Ricardo Frederiks <ricky2000@live.nl>
2016-12-30 18:08:53 +0100

    Removed white lines and added je oma

--- trest/Form1.cs
+++ trest/Form1.cs
@@ -21,8 +21,9 @@ namespace trest
         {
             // Je meoder123
             MessageBox.Show("Je moeder");
-
             MessageBox.Show("Je vader");
+            MessageBox.Show("Je oma");
         }
+
     }
 }
